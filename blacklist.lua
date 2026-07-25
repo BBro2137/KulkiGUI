00cfe3d4e8f8769d7ac48cf05f1ab5467323237c
BBro2137 <notrealbbro1@gmail.com>
2026-07-25 19:23:10 +0200

    Create blacklist.lua

---
+++ blacklist.lua
@@ -0,0 +1,9 @@
+--if youre reading this then dont bother trying to reverse these strings back to what they originally were, you will be caught and if i want to then you will also get banned from forsaken due to anti-cheat flagging on some next kulki gui execution. (yes there is another blacklist but hidden.)
+--have fun - BBro
+
+local array = {
+	"B3V3wgQ3h0inkGTVpPDULQ==",
+	"DkqsFv+bf+ZuAW8ChyTe4Q==",
+}
+
+return array
